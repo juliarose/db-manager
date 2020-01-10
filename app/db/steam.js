47d238e74c212ddd6d4c2bc19a135f633f2aac54
juliarose <julialunarose@gmail.com>
2020-01-10 12:02:47 -0500

    changed createDatabase to accept settings rather than path to settings

--- app/db/steam.js
+++ app/db/steam.js
@@ -1,9 +1,14 @@
 const createDatabase = require('../../lib/db/factory/createDatabase');
+const getSettings = require('../../lib/db/factory/utils/getSettings');
 const path = require('path');
-const cwd = process.cwd();
-const credentialsPath = path.join(cwd, 'config/mysql/steam/credentials.json');
-const schemaPath = path.join(cwd, 'config/mysql/steam/schema.json');
-const timePath = path.join(cwd, 'config/mysql/time.json');
-const {timezone} = require(timePath);
+const credentials = (function() {
+    const cwd = process.cwd();
+    const credentialsPath = path.join(cwd, 'config/mysql/steam/credentials.json');
+    const timePath = path.join(cwd, 'config/mysql/time.json');
+    const {timezone} = require(timePath);
+    const steamDbCredentials = require(credentialsPath);
+    
+    return getSettings(Object.assign(steamDbCredentials, {timezone}));
+}());
 
-module.exports = createDatabase({credentialsPath, schemaPath, timezone});
+module.exports = createDatabase(credentials);
